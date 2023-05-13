ab5ca982ffb5f383298c9b8dbc7bebd72e743bc3
Сергеєв Дмитро <sergeevdima017@gmail.com>
2023-05-13 14:13:50 +0300

    Jena topp

--- student_simulator.py
+++ student_simulator.py
@@ -1,2 +1,17 @@
 class Student:
-    print("Hello")
+    def __init__(self,name):
+        self.name=name
+        self.radist=1
+        self.life=True
+        self.pregress=1
+    def study(self):
+        print("📚ЧАС ДЛЯ НАВЧАННЯ📚")
+        self.radist-=0.5
+        self.pregress+=1
+    def chill(self):
+        print("🎮ЧАС ДЛЯ ДЕГРАДАЦІЇ🎮")
+        self.radist+=1
+        self.pregress-=0.5
+    def sleep(self):
+        print("☁ЧАС ДЛЯ СНУ☁")
+        self.radist+=1
