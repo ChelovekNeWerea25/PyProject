83f7ad28c7e8d3d32b57c99323e76828183d8ce3
Сергеєв Дмитро <sergeevdima017@gmail.com>
2023-05-27 14:01:09 +0300

    d

--- 13-37_27-05-2023.py
+++ 13-37_27-05-2023.py
@@ -1,7 +1,28 @@
 class Human:
-    day="Sybota"
-    def __init__(self,dz,raiting):
-        self.dz=dz
-        self.raiting=raiting
+    day = "Sybota"
+
+
+class Student(Human):
+    def __init__(self, dz, raiting):
+        self.dz = dz
+        self.raiting = raiting
+
     def print_student(self):
         print(f"В учня {self.dz} просрочок та {self.raiting} місце в рейтингу")
+
+
+class Dima(Student):
+    def __init__(self, hobby):
+        self.hobby = hobby
+
+    def printing(self):
+        print(f"Зохоплення студента це {self.hobby}")
+
+
+class Zenya(Human):
+    def __init__(self, group, mark):
+        self.group = group
+        self.mark = mark
+
+    def prntzenya(self):
+        print(f"Вчитель веде групу {self.group} і поставив середню оцінку {self.mark}")
