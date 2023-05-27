f3991cfc470520b1e086965f0cbc3ee5c12e8b1e
Сергеєв Дмитро <sergeevdima017@gmail.com>
2023-05-27 12:54:02 +0300

    Jena topp

--- lesson2.py
+++ lesson2.py
@@ -12,7 +12,20 @@ class Auto:
         for passenger in args:
             self.passengers.append(passenger)
     def print_passenger(self):
-        if(self.passengers):
-            print(f"В машині {self.brand} є такі пасажири:")
+        if self.passengers:
+            print(f"\nВ машині {self.brand} є такі пасажири:",end=" ")
             for passenger in self.passengers:
-                print(passenger.name)
+                print(passenger.name, end=" ")
+        else:
+            print(f"\nВ машині {self.brand} нікого немає")
+h1=Human("Женя")
+h2=Human("Сергей")
+h3=Human("Ярік")
+car1=Auto("Абобус")
+car2=Auto("Тесла")
+car3=Auto("Ауді")
+car1.add_passenger(h1,h2,h3)
+car2.add_passenger(h1,h3)
+car1.print_passenger()
+car2.print_passenger()
+car3.print_passenger()
