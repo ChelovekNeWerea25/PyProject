a15a5fbe9d191be260e92aee9796ef6f1da0a9cb
Сергеєв Дмитро <sergeevdima017@gmail.com>
2023-05-27 12:56:15 +0300

    nothing

--- lesson2.py
+++ lesson2.py
@@ -11,21 +11,24 @@ class Auto:
     def add_passenger(self, *args):
         for passenger in args:
             self.passengers.append(passenger)
+
     def print_passenger(self):
         if self.passengers:
-            print(f"\nВ машині {self.brand} є такі пасажири:",end=" ")
+            print(f"\nВ машині {self.brand} є такі пасажири:", end=" ")
             for passenger in self.passengers:
                 print(passenger.name, end=" ")
         else:
             print(f"\nВ машині {self.brand} нікого немає")
-h1=Human("Женя")
-h2=Human("Сергей")
-h3=Human("Ярік")
-car1=Auto("Абобус")
-car2=Auto("Тесла")
-car3=Auto("Ауді")
-car1.add_passenger(h1,h2,h3)
-car2.add_passenger(h1,h3)
+
+
+h1 = Human("Женя")
+h2 = Human("Сергей")
+h3 = Human("Ярік")
+car1 = Auto("Абобус")
+car2 = Auto("Тесла")
+car3 = Auto("Ауді")
+car1.add_passenger(h1, h2, h3)
+car2.add_passenger(h1, h3)
 car1.print_passenger()
 car2.print_passenger()
 car3.print_passenger()
