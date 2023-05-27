1531a8df88128a8bef36814bdafa8d973e5a6c3a
Сергеєв Дмитро <sergeevdima017@gmail.com>
2023-05-27 13:10:42 +0300

    Додав номерні знаки

--- lesson2.py
+++ lesson2.py
@@ -4,9 +4,10 @@ class Human:
 
 
 class Auto:
-    def __init__(self, brand, color):
+    def __init__(self, brand, color, number):
         self.brand = brand
         self.color = color
+        self.number = number
         self.passengers = []
 
     def add_passenger(self, *args):
@@ -15,19 +16,20 @@ class Auto:
 
     def print_passenger(self):
         if self.passengers:
-            print(f"\nВ машині {self.brand} колір якої '{self.color}' є такі пасажири:", end=" ")
+            print(f"\nВ машині {self.brand} з гос номером '{self.number}' колір якої '{self.color}' є такі пасажири:",
+                  end=" ")
             for passenger in self.passengers:
                 print(passenger.name, end=" ")
         else:
-            print(f"\nВ машині {self.brand} колір якої '{self.color}' нікого немає")
+            print(f"\nВ машині {self.brand} з гос номером '{self.number}' колір якої '{self.color}' нікого немає")
 
 
 h1 = Human("Женя")
 h2 = Human("Сергей")
 h3 = Human("Ярік")
-car1 = Auto("Абобус", "Жовтий")
-car2 = Auto("Тесла", "Білий")
-car3 = Auto("Ауді", "Чорний")
+car1 = Auto("Абобус", "Жовтий", "BI3587AG")
+car2 = Auto("Тесла", "Білий", "BI4576YY")
+car3 = Auto("Ауді", "Чорний", "BI7587CH")
 car1.add_passenger(h1, h2, h3)
 car2.add_passenger(h1, h3)
 car1.print_passenger()
