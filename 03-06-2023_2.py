b1c36fd27331d1f51b1b2efdf1eb48ba96e00f4b
Сергеєв Дмитро <sergeevdima017@gmail.com>
2023-06-03 14:29:21 +0300

    Додав кря

---
+++ 03-06-2023_2.py
@@ -0,0 +1,14 @@
+class duck:
+    def __init__(self):
+        self.name = "КРЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯ"
+
+    def sound(self):
+        print("Kra")
+
+
+class Human:
+    def __init__(self, name):
+        self.name = name
+
+    def sound(self):
+        print('Robit dz')
