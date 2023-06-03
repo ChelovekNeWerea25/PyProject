632babf383d7f59d01dbfd7b6750081d7a59cc9b
Сергеєв Дмитро <sergeevdima017@gmail.com>
2023-06-03 13:06:07 +0300

    Car added

--- 03-06-2023.py
+++ 03-06-2023.py
@@ -1,7 +1,7 @@
 class car:
     def __init__(self, marka, engine):
         self.marka = marka
-        self.__engine = engine
+        self.__engine = self.set_engine(engine)
 
     def printing(self):
         print(f"Марка машини {self.marka} в неї {self.__engine}л об'єм двигуна")
@@ -9,7 +9,14 @@ class car:
     def get_engine(self):
         return self.__engine
 
+    def set_engine(self, eng):
+        if eng >= 0:
+            self.__engine = eng
+        else:
+            print("Такого не буває")
+            self.__engine = eng / -1
 
-h = car("BMW", 23)
+
+h = car("BMW", -23)
 print(h.marka, h.get_engine())
 h.printing()
